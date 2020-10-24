253dee56728c69a82e8ad433c1389efab5b7ed89
GoldWires <67865290+GoldWires@users.noreply.github.com>
2020-10-24 15:26:14 +0300

    init: make function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix){
+    var masiv = [];
+    var n = 0;
+    for(var i = 0; i < matrix.length; i++){
+      if (i % 2 != 0) {
+        for(var j = matrix[i].length - 1; j >= 0; j--){
+        masiv[n] = matrix[i][j]; 
+        n++;
+        }
+      }
+      else{
+        for(var j = 0; j < matrix[i].length; j++){
+        masiv[n] = matrix[i][j]; 
+        n++;
+        }
+      }
+    }
+    return masiv;
+  }
+  else return [];
 }
